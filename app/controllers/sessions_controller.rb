0563e681c023b993ecb111836d7e4743e069280b
ryanbenedetti <rhino.j.benedetti@gmail.com>
2015-03-04 16:39:36 -0800

    Lesson 4 Sign-In: Form COmmit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+  	
+  end
+
+  def create
+   params[:email]
+   params[:password]	
+  end
+
+  # def edit
+  	
+  # end
+
+  # def update
+  	
+  # end
+
+  # def destroy
+  	
+  # end
+
+  # def show
+  	
+  # end
+
+
+end
